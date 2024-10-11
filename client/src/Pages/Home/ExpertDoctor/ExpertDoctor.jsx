3cad63a7d12830c428c0107df9e7f971debfa665
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-10-11 16:31:19 +0600

    contact section will start leter

--- client/src/Pages/Home/ExpertDoctor/ExpertDoctor.jsx
+++ client/src/Pages/Home/ExpertDoctor/ExpertDoctor.jsx
@@ -10,7 +10,10 @@ const ExpertDoctor = () => {
           "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et quis rem sed hic doloribus unde odio repudiandae accusantium accusamus in? Asperiores commodi natus sequi voluptatem rem quo dolore molestiae. Optio."
         }
       />
-      <div className="lg:mx-12 ">
+      <div className="lg:mx-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <DoctorCard />
+        <DoctorCard />
+        <DoctorCard />
         <DoctorCard />
       </div>
     </div>
