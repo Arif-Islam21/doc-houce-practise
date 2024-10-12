cdd71ff020c2a5f2e833524cb2c6eb680ac10299
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-10-12 12:27:59 +0600

    contact section done

--- client/src/Pages/Home/Contact/Contact.jsx
+++ client/src/Pages/Home/Contact/Contact.jsx
@@ -2,15 +2,18 @@ import { useState } from "react";
 import { CiLocationArrow1, CiPhone } from "react-icons/ci";
 import DatePicker from "react-datepicker";
 import TimePicker from "react-time-picker";
+import "react-datepicker/dist/react-datepicker.css";
+import moment from "moment";
 import "react-time-picker/dist/TimePicker.css";
 import "react-clock/dist/Clock.css";
+import Button from "../../../Components/Button/Button";
 
 const Contact = () => {
   const [startDate, setStartDate] = useState(new Date());
-  const [value, onChange] = useState(`10:00`);
+  const [time, setTime] = useState("10:00");
 
   return (
-    <div className="hero bg-primary my-6 rounded-md min-h-[70vh]">
+    <div className="hero bg-primary text-white my-6 rounded-md min-h-[70vh]">
       <div className="hero-content w-full md:px-16 flex-col lg:flex-row">
         <div className="text-center w-full md:w-1/3 lg:text-left">
           <h1 className="text-5xl font-bold">Contact With Us</h1>
@@ -30,7 +33,7 @@ const Contact = () => {
         </div>
         <div className="card bg-primary w-full md:w-2/3 shrink-0">
           <form className="card-body">
-            <div className="form-control grid grid-cols-1 gap-4 md:grid-cols-2 ">
+            <div className="form-control grid grid-cols-1 space-y-3 gap-4 md:grid-cols-2 ">
               <input
                 type="text"
                 placeholder="Name"
@@ -59,13 +62,24 @@ const Contact = () => {
                 selected={startDate}
                 dateFormat="yyyy/MM/dd"
                 onChange={(date) => setStartDate(date)}
-                className="input bg-[#133D39] text-white font-bold input-bordered w-full"
+                className="input bg-[#133D39] p-2 text-white font-bold input-bordered w-full"
                 withPortal
               />
-              <TimePicker onChange={onChange} value={value} />
+              <TimePicker
+                onChange={(newTime) => {
+                  setTime(newTime);
+                  console.log(
+                    "Selected Time:",
+                    moment(newTime, "HH:mm").format("HH:mm:ss")
+                  );
+                }}
+                value={time}
+                disableClock
+                className="input bg-[#133D39] text-gray-200 font-bold border-0 w-full"
+              />
             </div>
             <div className="form-control mt-6">
-              <button className="btn btn-primary">Login</button>
+              <Button text={"Book Now"} type={"secondary"} />
             </div>
           </form>
         </div>
