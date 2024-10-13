160e094b81789f9c8272f66d3de7557ab43c1918
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-10-13 22:34:55 +0600

    modal setup done

--- client/src/Components/Button/Button.jsx
+++ client/src/Components/Button/Button.jsx
@@ -2,9 +2,14 @@ import { AwesomeButton } from "react-awesome-button";
 import "react-awesome-button/dist/styles.css";
 import "./button.css";
 
-const Button = ({ text, type, width }) => {
+const Button = ({ children, onPress, text, type, width }) => {
   return (
-    <AwesomeButton className={`rounded-full ${width && "w-full"}`} type={type}>
+    <AwesomeButton
+      onPress={onPress}
+      className={`rounded-full ${width && "w-full"}`}
+      type={type}
+    >
+      {children}
       {text}
     </AwesomeButton>
   );
--- client/src/Pages/Appoinement/AvailableSlots/AvailableCard.jsx
+++ client/src/Pages/Appoinement/AvailableSlots/AvailableCard.jsx
@@ -2,9 +2,17 @@ import { useState } from "react";
 import image from "../../../assets/dental/image2.jfif";
 import Button from "../../../Components/Button/Button";
 import { Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
+import AppoineModal from "../../../Components/AppoinementModal/AppoineModal";
 
 const AvailableCard = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const handlePopUp = () => {
+    setIsOpen(true);
+  };
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
   return (
     <div className="card bg-base-100 shadow-2xl">
       <figure>
@@ -18,9 +26,14 @@ const AvailableCard = () => {
         <h2 className="card-title">Teeth Orthodontics</h2>
         <p>8:00AM - 9:00PM</p>
         <div className="card-actions justify-center">
-          <Button text="Book Appoinement" type="secondary" />
+          <Button
+            onPress={handlePopUp}
+            text="Book Appoinement"
+            type="secondary"
+          />
         </div>
       </div>
+      <AppoineModal handleClose={handleClose} isOpen={isOpen} />
     </div>
   );
 };
