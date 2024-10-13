efbd6fe56cfa94823f2e4cf0629915b01c34fb85
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-10-13 15:52:18 +0600

    working with service section

--- client/src/Pages/Appoinement/SelectAppoint.jsx
+++ client/src/Pages/Appoinement/SelectAppoint.jsx
@@ -5,6 +5,7 @@ import DatePicker from "react-datepicker";
 
 const SelectAppoint = () => {
   const [startDate, setStartDate] = useState(new Date());
+
   return (
     <div
       style={{
@@ -15,17 +16,26 @@ const SelectAppoint = () => {
       className="min-h-[80vh] hero"
     >
       <div className="hero-overlay bg-white bg-opacity-80"></div>
-      <div className="flex flex-col md:flex-row gap-12">
-        <div className="w-full flex justify-end items-start md:w-1/2">
-          <DatePicker
-            selected={startDate}
-            onChange={(date) => setStartDate(date)}
-            open
-            className="input bg-gray-300 shadow-lg"
-          />
+
+      <div className="">
+        <div className="flex flex-col md:flex-row gap-12">
+          <div className="w-full flex justify-end items-start md:w-1/2">
+            <DatePicker
+              selected={startDate}
+              onChange={(date) => setStartDate(date)}
+              open
+              className="input bg-gray-300 shadow-lg"
+            />
+          </div>
+          <div className="w-full md:w-1/2">
+            <img src={image} className="rounded-md" alt="" />
+          </div>
         </div>
-        <div className="w-full md:w-1/2">
-          <img src={image} className="rounded-md" alt="" />
+        <div className="mt-8 text-center">
+          <h5 className="text-secondary font-bold">
+            Available sit On {startDate.toDateString()}
+          </h5>
+          <h2 className="text-4xl font-bold">Please Select a service</h2>
         </div>
       </div>
     </div>
--- client/src/Routes/Router.jsx
+++ client/src/Routes/Router.jsx
@@ -22,7 +22,7 @@ const router = createBrowserRouter([
         element: <DoctorHome />,
       },
       {
-        path: "/appointment",
+        path: "/Appointment",
         element: <Appointment />,
       },
     ],
